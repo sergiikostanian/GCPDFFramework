e7bc909b833f4d710196bae761953435dd818dbf
Sergii Kostanian <sk@madappgang.com>
2015-11-04 18:14:26 +0200

    wwww

--- prepare_build_phase_script.rb
+++ prepare_build_phase_script.rb
@@ -10,6 +10,7 @@ path_to_project = "ScriptTest.xcodeproj"
 project = Xcodeproj::Project.open(path_to_project)
 
 main_target = project.targets.first
+puts main_target.name
 phase = main_target.new_shell_script_build_phase("GC Script")
 phase.shell_script = ""
 project.save()
