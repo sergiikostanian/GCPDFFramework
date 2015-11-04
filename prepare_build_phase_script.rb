b2e371d6e38e407959b195113c4d2b550aec29cd
Sergii Kostanian <sk@madappgang.com>
2015-11-04 17:39:32 +0200

    rb fix try #5

--- prepare_build_phase_script.rb
+++ prepare_build_phase_script.rb
@@ -1,11 +1,11 @@
 require 'xcodeproj'
 
-current_pwd="$PWD"
-project_dir=`cd "../../"; pwd`
+#current_pwd="$PWD"
+#project_dir=`cd "../../"; pwd`
 #cd "$current_pwd"
-path_to_project=`find "$project_dir" -maxdepth 1 -name "*.xcodeproj" | tail -1`
+#path_to_project=`find "$project_dir" -maxdepth 1 -name "*.xcodeproj" | tail -1`
 
-#path_to_project = "${SRCROOT}/${PROJECT_NAME}.xcodeproj"
+path_to_project = "${SRCROOT}/${PROJECT_NAME}.xcodeproj"
 
 project = Xcodeproj::Project.open(path_to_project)
 
