c0a254532453c2f88aa5e77d0a32dacfb07ea1e1
Sergii Kostanian <sk@madappgang.com>
2015-11-04 18:49:47 +0200

    sdc

--- prepare_build_phase_script.rb
+++ prepare_build_phase_script.rb
@@ -1,16 +1,15 @@
 require 'xcodeproj'
 
-#current_pwd="$PWD"
-#project_dir=`cd "../../"; pwd`
-#cd "$current_pwd"
-#path_to_project=`find "$project_dir" -maxdepth 1 -name "*.xcodeproj" | tail -1`
+current_pwd="$PWD"
+project_dir=`cd "../../"; pwd`
+cd "$current_pwd"
+path_to_project=`find "$project_dir" -maxdepth 1 -name "*.xcodeproj" | tail -1`
 
-path_to_project = "ScriptTest.xcodeproj"
+#path_to_project = "ScriptTest.xcodeproj"
 
 project = Xcodeproj::Project.open(path_to_project)
 
 main_target = project.targets.first
-puts main_target.name
 phase = main_target.new_shell_script_build_phase("GC Script")
 phase.shell_script = ""
 project.save()
